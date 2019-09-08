3a89f7923c64a8fb9fbdda02be1ecbe18016cdad
Ganeshk750 <ganesh9ot@gmail.com>
2019-09-08 19:41:04 +0530

    Adding Spring security dpendsy

---
+++ src/main/java/com/ganesh/DemoSqrtySpringApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSqrtySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSqrtySpringApplication.class, args);
+	}
+
+}
